1d988141d2f7bac6603eee8ed72e1ffe6837fa40
masrayiemas <rayiemas.8b@gmail.com>
2018-05-06 16:56:13 +0700

    tabel jdawal

--- application/views/tabeljadwalview.php
+++ application/views/tabeljadwalview.php
@@ -12,7 +12,7 @@
                             <h3 class="card-title"> Input Jadwal Pelajaran</h3>
                           </div>
                           <div class="col-md-2 stretch-card">
-                              <a  href="<?php echo base_url('index.php/JadwalSeluruh/viewSettingShift'); ?>" style=" padding:15px 13px;" class="btn btn-danger mr-2"> <i class="mdi mdi-settings"></i><h7 class="card-title">Set Shift</h7></a>
+                              <h7 class="card-title"><a  href="<?php echo base_url('index.php/JadwalSeluruh/viewSettingShift'); ?>" class="btn btn-danger "> <i class="mdi mdi-settings"></i>Set Shift</a></h7>
                           </div>
                       </div>
                         <div class="row" style="margin-bottom:-10px; margin-top:10px;">
@@ -61,14 +61,20 @@
                                       </select>
                                 </div>
                             </div>
-                            <div class="col-md-1" id="rowjumkelas">
+                            <div class="col-sm-2" id="rowjumkelas" style="max-width:12.5%;">
                                 <div class="form-group">
-                                      <input type="number" min="1" class="form-control form-control-sm" name="jumlah_kelas" id="jumlahkelas" placeholder="Jumlah Kelas" style="padding:6px 5px; border: 1px solid #aaa;" />
+                                      <input type="number" min="1" class="form-control form-control-sm" name="jumlahkelas" id="jumlahkelas" placeholder="Jumlah Kelas" style="padding:6px 5px; border: 1px solid #aaa;" />
                                 </div>
                             </div>
-                            <div class="col-sm-1">
+                            <div class="col-sm-2" id="rowjumbaris" style="max-width:12.5%;">
                                 <div class="form-group">
-                                      <button type="button" class="btn btn-primary btn-xs" onclick="outputTabel()">Go !</button>
+                                      <input type="number" min="1" class="form-control form-control-sm" name="jumlahbaris" id="jumlahkelas" placeholder="Baris Jadwal" style="padding:6px 5px; border: 1px solid #aaa;" />
+                                </div>
+                            </div>
+                            <div class="col-sm-2">
+                                <div class="form-group">
+                                  <button type="button" class="btn btn-primary btn-xs" onclick="outputTabel()">Go !</button>
+                                  <button type="button" id="btnreset" class="btn btn-outline-warning btn-xs" onclick="reset()"><i class="mdi mdi-restart"></i>Reset</button>
                                 </div>
                             </div>
                           </form>
@@ -96,10 +102,27 @@
           <script src="<?php echo base_url("assets/node_modules/jquery/dist/jquery.min.js");?>"></script>
           <script src="<?php echo base_url("assets/vendor/Select2/dist/js/select2.min.js")?>"></script>
         <script type="text/javascript">
+          $(window).bind('beforeunload', function(){
+            localStorage.removeItem("jumlahkelas");
+          });
+          $(document).ready(function(){
+            // if(!localStorage.getItem("jumlahkelas")){
+            //   alert("gaadacuk");
+            // } else{
+            //   alert("ganok");
+            // }
+            // localStorage.removeItem("jumlahkelas");
+          });
+
+          //Hidden rowmi, rowmts, rowjumkelas, rowbtnsubmit
           document.getElementById("rowmi").style.display="none";
           document.getElementById("rowmts").style.display="none";
           document.getElementById("rowjumkelas").style.display="none";
+          document.getElementById("rowjumbaris").style.display="none";
           document.getElementById("rowbtnsubmit").style.display="none";
+          document.getElementById("btnreset").style.display="none";
+
+          //select2 declaration
           $("#selecthariku").select2({
             placeholder: "Pilih Hari",
             allowClear : true,
@@ -110,6 +133,27 @@
             allowClear : true,
           });
 
+          $("#selectmapelku").select2({
+            placeholder: "Pilih Matapelajaran",
+            allowClear : true,
+          });
+
+          $("#selecttingkatmi").select2({
+            placeholder: "Pilih Tingkat",
+            allowClear : true,
+          });
+
+          $("#selecttingkatmts").select2({
+            placeholder: "Pilih Tingkat",
+            allowClear : true,
+          });
+
+          $("#selectshift").select2({
+            placeholder: "Pilih Shift",
+            allowClear : true,
+          });
+
+          //handler select2
           $("#selecthariku").on("change", function(){
               $("#selectjenjangku").removeAttr("disabled");
           });
@@ -121,13 +165,16 @@
                 $("#rowmi").hide();
                 $("#rowmts").hide();
                 $("#rowjumkelas").show();
+                $("#rowjumbaris").show();
               } else if(jenjang == 1){
                 $("#rowmi").show();
                 $("#rowmts").hide();
+                $("#rowjumbaris").show();
                 $("#rowjumkelas").show();
               } else {
                 $("#rowmts").show();
                 $("#rowmi").hide();
+                $("#rowjumbaris").show();
                 $("#rowjumkelas").show();
               }
           });
@@ -155,11 +202,31 @@
               }
           ];
 
+          function cekLocalStorage(){
+            if(!localStorage.getItem("jumlahkelas")){
+              alert("belum ada");
+            } else{
+              alert(localStorage.getItem("jumlahkelas"));
+            }
+          }
+
+          //function output tabel dinamis
           function outputTabel(){
-            var hari, jenjang, jenjang2, tingkat, jumlahkelas, alphabet
+            var hari, jenjang, jenjang2, tingkat, jumlahkelas, alphabet, kelasstorage
             hari        = document.getElementById("selecthariku").value;
             jenjang     = document.getElementById("selectjenjangku").value;
             jumlahkelas = document.getElementById("jumlahkelas").value;
+            $("#btnreset").show();
+
+            if(!localStorage.getItem("jumlahkelas")){
+                kelasstorage = jumlahkelas;
+                localStorage.setItem("jumlahkelas", kelasstorage);
+                // alert(kelasstorage);
+            } else{
+                kelasstorage = parseInt(localStorage.getItem("jumlahkelas")) + parseInt(jumlahkelas);
+                localStorage.setItem("jumlahkelas", kelasstorage);
+                // alert("dari localstorage = " + localStorage.getItem("jumlahkelas"));
+            }
 
             if(jenjang == 0){
               jenjang2 = "Taman Kanak-Kanak";
@@ -172,27 +239,33 @@
             }
 
             alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
-
+            if(kelasstorage==jumlahkelas){
+                counter = 0;
+            } else {
+                counter = kelasstorage-jumlahkelas;
+                alert(counter);
+            }
+            // alert(counter);
             for (var i = 0; i < jumlahkelas; i++) {
                 if(jenjang == 0){
-                    $("#tabeldata").append("<div class='col-lg-12 grid-margin stretch-card' style='margin-bottom:-20px;'> <div class='card'> <div class='card-body'> <h4 class='card-title'>Jadwal <i>"+ jenjang2 +"</i>,  Hari "+ hari +" </h4> <table class='table table-bordered' id='tabeljadwal'> <thead> <tr style='vertical-align:top;'> <th width='10%'>ID Jadwal</th> <th width='20%'>Matapelajaran</th> <th width='15%'>Kode Guru</th> <th width='12%'>Shift</th> <th width='5%'>Jam Mulai</th> <th width='5%'>Jam Selesai</th> <th width='15%'>Keterangan</th> <th width='15%'>Operation</th> </tr> </thead> <tbody> <tr> <td> </td> <td> <select id='selectmapelku"+ i +"' class='col-sm-12'> <option></option> </select> </td> <td> <select id='selectguru"+ i +"' class='col-sm-12' name='selectguru'><option></option></select> </td> <td> <select id='selectshift"+i+"'name='selectshift' class='col-sm-12'> <option></option> </select> </td> <td></td> <td></td> <td></td> <td>tes</td> </tr> </tbody> </table> </div> </div> </div>");
+                    $("#tabeldata").append("<div class='col-lg-12 grid-margin stretch-card' style='margin-bottom:-20px;'> <div class='card'> <div class='card-body'> <h4 class='card-title'>Jadwal <i>"+ jenjang2 +"</i>,  Hari "+ hari +" Kelas "+ alphabet[(counter+i)] +" </h4> <table class='table table-bordered' name='jadwaltabel' id='tabeljadwal'> <thead> <tr style='vertical-align:top;'> <th width='10%'>ID Jadwal</th> <th width='20%'>Matapelajaran</th> <th width='15%'>Kode Guru</th> <th width='12%'>Shift</th> <th width='5%'>Jam Mulai</th> <th width='5%'>Jam Selesai</th> <th width='15%'>Keterangan</th> <th width='15%'>Operation</th> </tr> </thead> <tbody> <tr> <td> </td> <td> <select id='selectmapelku"+ (counter+i) +"' class='col-sm-12'> <option></option> </select> </td> <td> <select id='selectguru"+ (counter+i) +"' class='col-sm-12' name='selectguru'><option></option></select> </td> <td> <select id='selectshift"+(counter+i)+"'name='selectshift' class='col-sm-12'> <option></option> </select> </td> <td></td> <td></td> <td></td> <td>tes</td> </tr> </tbody> </table> </div> </div> </div>");
                 } else {
-                    $("#tabeldata").append("<div class='col-lg-12 grid-margin stretch-card' style='margin-bottom:-20px;'> <div class='card'> <div class='card-body'> <h4 class='card-title'>Jadwal <i>"+ jenjang2 +"</i>,  Hari "+ hari +",  Kelas "+ tingkat + "-" + alphabet[i] +" </h4> <table class='table table-bordered' id='tabeljadwal'> <thead> <tr style='vertical-align:top;'> <th width='10%'>ID Jadwal</th> <th width='20%'>Matapelajaran</th> <th width='15%'>Kode Guru</th> <th width='10%'>Shift</th> <th width='5%'>Jam Mulai</th> <th width='5%'>Jam Selesai</th> <th width='15%'>Keterangan</th> <th width='15%'>Operation</th> </tr> </thead> <tbody> <tr> <td> </td> <td> <select id='selectmapelku"+ i +"' class='col-sm-12'> <option></option> </select> </td> <td><select id='selectguru"+ i +"' class='col-sm-12' name='selectguru'><option></option></select></td> <td> <select name='selectshift' id='selectshift"+ i +"' class='col-sm-12'> <option></option> </select> </td> <td></td> <td></td> <td></td> <td>tes</td></tr> </tbody> </table> </div> </div> </div>");
+                    $("#tabeldata").append("<div class='col-lg-12 grid-margin stretch-card' style='margin-bottom:-20px;'> <div class='card'> <div class='card-body'> <h4 class='card-title'>Jadwal <i>"+ jenjang2 +"</i>,  Hari "+ hari +",  Kelas "+ tingkat + "-" + alphabet[(counter+i)] +" </h4> <table class='table table-bordered' id='tabeljadwal'> <thead> <tr style='vertical-align:top;'> <th width='10%'>ID Jadwal</th> <th width='20%'>Matapelajaran</th> <th width='15%'>Kode Guru</th> <th width='10%'>Shift</th> <th width='5%'>Jam Mulai</th> <th width='5%'>Jam Selesai</th> <th width='15%'>Keterangan</th> <th width='15%'>Operation</th> </tr> </thead> <tbody> <tr> <td> </td> <td> <select id='selectmapelku"+ (counter+i) +"' class='col-sm-12'> <option></option> </select> </td> <td><select id='selectguru"+ (counter+i) +"' class='col-sm-12' name='selectguru'><option></option></select></td> <td> <select name='selectshift' id='selectshift"+ (counter+i) +"' class='col-sm-12'> <option></option> </select> </td> <td></td> <td></td> <td></td> <td>tes</td></tr> </tbody> </table> </div> </div> </div>");
                 }
 
                 $("#rowbtnsubmit").show();
 
-                $("#selectmapelku"+i).select2({
+                $("#selectmapelku"+(counter+i)).select2({
                   placeholder: "Pilih Matapelajaran",
                   allowClear : true,
                   data : data
                 });
-                $("#selectshift"+i).select2({
+                $("#selectshift"+(counter+i)).select2({
                   placeholder: "Pilih Shift",
                   allowClear : true,
                   data : data
                 });
-                $("#selectguru"+i).select2({
+                $("#selectguru"+(counter+i)).select2({
                   placeholder: "Pilih Kode Guru",
                   allowClear : true,
                   data : data
@@ -201,25 +274,6 @@
 
             }
 
-            $("#selectmapelku").select2({
-              placeholder: "Pilih Matapelajaran",
-              allowClear : true,
-            });
-
-            $("#selecttingkatmi").select2({
-              placeholder: "Pilih Tingkat",
-              allowClear : true,
-            });
-
-            $("#selecttingkatmts").select2({
-              placeholder: "Pilih Tingkat",
-              allowClear : true,
-            });
-
-            $("#selectshift").select2({
-              placeholder: "Pilih Shift",
-              allowClear : true,
-            });
         </script>
         <!-- partial -->
       </div>
